4471f54d21e2779894b59e87c518518cef8ca3c6
Bernarda Gonçalves <ireneteresa.13@gmail.com>
2025-02-17 19:59:19 +0100

    Processador de dados

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var Reader = require("./Reader")
+var Processor = require("./Processor")
 
 var leitor = new Reader()
 
 async function main(){
     var dados = await leitor.Read("./users.csv")
-    console.log(dados)
+
+    var dadosProcessados =  Processor.Process(dados)
+    
 }
 
 main()
