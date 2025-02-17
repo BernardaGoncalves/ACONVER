9c417265ad15dd798b923cb5fd5eed29746f674a
Bernarda Gonçalves <ireneteresa.13@gmail.com>
2025-02-17 19:44:08 +0100

    Fim da leitura

--- Reader.js
+++ Reader.js
@@ -1,17 +1,18 @@
 const fs = require("fs")
+const util = require("util")
 
 class Reader{
 
-    Read(filepath){
+    constructor(){
+        this.reader = util.promisify(fs.readFile)
+    }
 
-        fs.readFile(filepath, "utf-8", (erro, data) => {
-            if(erro){
-                console.log(erro)
-            }else {
-                console.log(data)
-            }
-        })
-
+    async Read(filepath){
+        try{
+            return await this.reader(filepath, "utf8")
+        }catch(err){
+            return undefined
+        }
     }
 }
 
